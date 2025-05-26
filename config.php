a2c11dbf40c6d8e3da6d2e94d820bb527cd2c227
Ayush Kumar <168015232+Ayushkr093@users.noreply.github.com>
2025-05-26 17:55:44 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'mysql-app');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'admin');
+define('DB_SERVER', 'mysql');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Testing');
 define('DB_NAME', 'demo_db');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($link === false) {
